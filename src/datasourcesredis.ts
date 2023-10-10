9eacec701a9903039b539d034b01b5e417197783
JF Jobidon <jfjobidon@gmail.com>
2023-10-10 11:17:59 -0400

    refactoring addVote

--- src/datasourcesredis.ts
+++ src/datasourcesredis.ts
@@ -31,53 +31,38 @@ const voteSchema = new Schema('vote', {
 
 let voteRepository = new Repository(voteSchema, redisClient)
 // await voteRepository.dropIndex();
-await voteRepository.createIndex();  // required to use search
+await voteRepository.createIndex();  // required to use search (RediSearch)
 
 export class DataSourcesRedis {
-
   // async addVote(voterID: string, invoice: string, date: number, campaignID: string, certified: boolean) {
-  async addVote({ voterID, invoice, date, campaignID, pollID, certified }: Vote): Promise<AddVoteMutationResponse> {
-    console.log("in addvote...")
-    // let vote = await voteRepository.save('myEntityId', { // key = vote:myEntityId
-    console.log(`voterID: ${voterID}`)
-    console.log(`invoice: ${invoice}`)
-    console.log(`date: ${date}`)
-    console.log(`campaignID: ${campaignID}`)
-    console.log(`pollID: ${pollID}`)
-    console.log(`certified: ${certified}`)
-    let vote = await voteRepository.save({
-      voterID,
-      invoice,
-      date,
-      campaignID,
-      pollID,
-      certified
-    });
-    console.log('vote: ' + vote)
-    console.table(vote)
-    console.log('vote EntityId: ' + vote[EntityId])
-    console.log('vote EntityId: ' + vote[EntityId])
-
-    let voteReturned = {
-      voterID,
-      invoice,
-      date,
-      campaignID,
-      pollID,
-      certified
-    }
-    console.table(voteReturned)
-    return {
-      code: "200",
-      success: true,
-      message: "New vote added!",
-      vote: voteReturned, // TODO: voteReturned --> vote[EntityId]
+  // async addVote({ voterID, invoice, date, campaignID, pollID, certified }: Vote): Promise<AddVoteMutationResponse> {
+  async addVote(theVote: Vote): Promise<AddVoteMutationResponse> {
+    let vote = await voteRepository.save(theVote);
+    // console.table(vote)
+    // console.log('vote EntityId: ' + vote[EntityId])
+    const exists = await redisClient.exists(`vote:${vote[EntityId]}`)
+    if (exists) {
+      return {
+        code: "200",
+        success: true,
+        message: "New vote added!",
+        vote: Object(vote), // vote is of type Symbol
+      }
+    } else {
+      return {
+        code: "400",
+        success: false,
+        message: "Problem adding new vote!",
+        vote: null
+      }
     }
   }
 
-  //  async getVotes(): Promise<{votes: String}>{
   async getVotes(): Promise<GetVotesQueryResponse> {
     console.log("in getVotes...")
+
+    // const votesNb: number = await voteRepository.search().return.count()
+    // console.log(`Number of votes: ${votesNb}`)
 
     const allVotes = await voteRepository.search()
     // .where('date').matches('date01')  // type must be 'text'
@@ -86,43 +71,23 @@ export class DataSourcesRedis {
     // .and('year').is.greaterThan(2000)
     .return.all()
 
-    // OK
-    // const exists = await redisClient.exists('vote:01HCAK7PMK48C61M9ME93J3WJ6')
-    // console.log('exists: ' + exists);
-
-    //set an entity to expire after a certain number of seconds
-    // const ttlInSeconds = 12 * 60 * 60  // 12 hours
-    // await albumRepository.expire('01FJYWEYRHYFT8YTEGQBABJ43J', ttlInSeconds)
-
-    // const voteX = await voteRepository.fetch('aaaa')
-    // console.log('new vote')
-    // console.table(voteX)
-    // voteX.date = 'updating date'
-    // voteRepository.save(voteX)  // will update the document
-    // // pour créer une nouvelle entrée: changer le entity ID
-    // console.log('new voteX')
-    // console.table(voteX)
-
-
-    // If you have RediSearch installed on your Redis server
-    //   // await voteRepository.createIndex()
-    // const allVotes = await voteRepository.search().returnAll()
-    //   let allVotes = await voteRepository.fetch()
-    // console.log('getVotes: ' + JSON.stringify(allVotes, null, 2))
     console.table(allVotes)
     console.log('getVotes entity: ' + allVotes[EntityId])
-    //   // console.log('votes2: ' + allVotes)
-    // return { votes: "all votes" }
-    return allVotes[EntityId]
+
+    let votesResponse: Vote[] = allVotes.map(x => Object(x)) // convert [Entity] to [Vote]  // REVIEW: send [Entity]
+    // https://github.com/redis/redis-om-node/blob/main/README.md
+    return {votes: votesResponse}
   }
 
-  // async getVotes(): Promise<GetVotesQueryResponse>{
-  // async getVotes(): Promise<{ votes: String }>{
-  //   console.log("in addvote...")
-  //   let votes = voteRepository.fetch();
-  //   console.log(votes)
-  //   return {
-  //     votes: "votes",
-  //   }
-  // }
+  async getVote(voteID: string): Promise<Vote>{
+    // const exists = await redisClient.exists(voteID)
+    const exists = await redisClient.exists(`vote:${voteID}`)
+    if (exists) {
+      let vote = await voteRepository.fetch(voteID)
+      // let vote = await voteRepository.fetch('vote:01HCAK7PMK48C61M9ME93J3WJ6')
+      return Object(vote)
+    } else {
+      return null
+    }
+  }
 }
