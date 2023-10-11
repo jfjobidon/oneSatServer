7163da2ee111c54a69590049c563151d6dee4126
JF Jobidon <jfjobidon@gmail.com>
2023-10-11 14:43:52 -0400

    new subs file resolver

---
+++ src/resolvers/subscriptions.ts
@@ -0,0 +1,16 @@
+// import { SubscriptionResolvers } from "../__generated__/resolvers-types";
+// import { PubSub } from 'graphql-subscriptions';
+// const pubsub = new PubSub()
+
+// const subscriptions: SubscriptionResolvers = {
+//     voteAdded: {
+//         // subscribe: () => pubsub.asyncIterator(['EVENT_VOTEADDED'])
+//         // https://github.com/apollographql/apollo-server/issues/4556
+//         subscribe: () => ({
+//             [Symbol.asyncIterator]: () => pubsub.asyncIterator(['EVENT_VOTEADDED']),
+//         }),
+//     }
+// }
+
+// export default subscriptions;
+export {}
