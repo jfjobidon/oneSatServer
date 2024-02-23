6efc280872156b99e6b178591b58c15f35955bb6
JF Jobidon <jfjobidon@gmail.com>
2024-02-23 10:22:54 -0500

    test3 ok

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// NOTE3: pour merger cette branche sur master et écraser master
+// NOTE: pour merger cette branche sur master et écraser master
 // https://javaetmoi.com/2013/08/ecraser-une-branche-par-une-autre-avec-git/
 
 import { ApolloServer } from '@apollo/server';
